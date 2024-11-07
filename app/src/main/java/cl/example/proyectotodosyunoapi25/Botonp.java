ee2886d2b997f8f8b1466868594d42cf337fcafd
javers1 <javiermapache17@gmail.com>
2024-11-06 22:23:41 -0300

    Actualizado botonp.java

--- app/src/main/java/cl/example/proyectotodosyunoapi25/Botonp.java
+++ app/src/main/java/cl/example/proyectotodosyunoapi25/Botonp.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -32,20 +31,53 @@ public class Botonp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.botonp);
-
-        ImageButton botonretroceder = (ImageButton) findViewById(R.id.backbutton);
-        botonretroceder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Botonp.this, Menu.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.botonp), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-}}
+
+        Button panico = findViewById(R.id.botonpanico);
+        panico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                subirBoton_panico();
+                Intent botonP_activado = new Intent(Botonp.this, android.R.menu.class);
+                startActivity(botonP_activado);
+
+            }
+        });
+
+
+
+
+    }
+
+    private void subirBoton_panico() {
+
+        String id = UUID.randomUUID().toString();
+
+        Map<String, Object> boton_panico = new HashMap<>();
+        boton_panico.put("id", id);
+
+        mStore.collection("BotonPanico").document(id).set(boton_panico)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        Toast.makeText(Botonp.this,"Aviso subido exitosamente", Toast.LENGTH_LONG).show();
+
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(Botonp.this,"Aviso no se pudo subir", Toast.LENGTH_LONG).show();
+
+
+                    }
+                });
+    }}
