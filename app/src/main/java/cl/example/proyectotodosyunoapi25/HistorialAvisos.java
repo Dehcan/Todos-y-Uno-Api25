ead5ca14cd119d032e7526a08a80a25b50c639f1
Dehcan <ale123774@gmail.com>
2024-11-25 08:02:49 -0300

    Update HistorialAvisos.java

--- app/src/main/java/cl/example/proyectotodosyunoapi25/HistorialAvisos.java
+++ app/src/main/java/cl/example/proyectotodosyunoapi25/HistorialAvisos.java
@@ -26,6 +26,7 @@ public class HistorialAvisos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.historial_avisos);
 
         // Inicializar RecyclerView
